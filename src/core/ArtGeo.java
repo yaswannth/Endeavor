f67ff2049b65a89a6e27bc46284d93295165c204
yaswannth <yaswannth.k@gmail.com>
2016-03-02 02:41:56 -0700

    Added Menu's UI elements without functionality

--- src/core/ArtGeo.java
+++ src/core/ArtGeo.java
@@ -14,10 +14,49 @@ public class ArtGeo extends JFrame{
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private JPanel menuPane;
+	private JMenuBar menuBar;
+	private JMenu fileMenu;
+	private JMenu helpMenu;
+	
+	private JMenuItem openFile;
+	private JMenuItem saveFile;
+	private JMenuItem clearAll;
+	private JMenuItem exit;
+	
+	private JMenuItem about;
 	
 	public ArtGeo(){
+		
+		menuPane = new JPanel(new BorderLayout());
+		menuBar = new JMenuBar();		
+		fileMenu = new JMenu("File");
+		helpMenu = new JMenu("Help");
+		
+		openFile = new JMenuItem("Open File");
+		saveFile = new JMenuItem("Save File");
+		clearAll = new JMenuItem("Clear All");
+		exit = new JMenuItem("Exit");
+		about = new JMenuItem("About");
+		
+		fileMenu.add(openFile);
+		fileMenu.add(saveFile);
+		fileMenu.addSeparator();
+		fileMenu.add(clearAll);
+		fileMenu.addSeparator();
+		fileMenu.add(exit);
+		
+		helpMenu.add(about);
+		
+		menuBar.add(fileMenu);
+		menuBar.add(helpMenu);
+		menuPane.setBackground(Color.WHITE);
+		menuPane.add(menuBar);
+		
+		
 		setTitle("Geo Art");
 		setSize(500, 500);	
+		getContentPane().add(menuPane,BorderLayout.NORTH);
 		setVisible(true);	
 		setDefaultCloseOperation(EXIT_ON_CLOSE);		
 	}
