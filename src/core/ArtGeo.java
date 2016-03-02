a9ef7c6bec8c41e4a8c367fbf38fe55a2cab425d
yaswannth <yaswannth.k@gmail.com>
2016-03-02 01:20:00 -0700

    Used swing to display a counter with a button

--- src/core/ArtGeo.java
+++ src/core/ArtGeo.java
@@ -1,44 +1,59 @@
 package core;
 
-import java.awt.Font;
-import java.awt.Graphics;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.*; //Using AWT containers and components
+import java.awt.event.*; //Using AWT events and listener interfaces
+import javax.swing.*; //Using Swing components and containers
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class ArtGeo extends JPanel{
+//A Swing GUI app inherits from top-level container
+//javax.swing.JFrame
+public class ArtGeo extends JFrame{
+	private static final long serialVersionUID = 2506635311264141215L;
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
-	String[] type = {"Serif", "SansSerif"};
-	int[] styles = {Font.PLAIN, Font.ITALIC, Font.BOLD, Font.ITALIC + Font.BOLD};
-	String[] stylenames = { "Plain", "Italic", "Bold", "Bold & Italic" };
 	
-	public void paint(Graphics g) {
-		for ( int f = 0; f < type.length; f++) {
-			for ( int s = 0; s < styles.length; s++) {
-				Font font = new Font(type[f], styles[s], 18);
-				g.setFont(font);
-				String name = type[f] + " " + stylenames[s];
-				g.drawString(name, 20, (f * 4 + s + 1) * 20);
-			}
-		}
-	}
+	private JTextField tfCount; //Using Swing's JTextField
+	private int count = 0;
 	
-	public static void main(String[] args){
-		JFrame f = new JFrame();
-		f.addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
+	/** Constructor to setup the GUI */
+	public ArtGeo () {
+		//Retrieve the content-pane of the top-level container JFrame
+		//All operations are one on the content pane
+		Container cp = getContentPane();
+		cp.setLayout(new FlowLayout());
+		
+		cp.add(new JLabel("Counter"));
+		tfCount = new JTextField("0", 10);
+		tfCount.setEditable(false);
+		cp.add(tfCount);
+		
+		JButton btnCount = new JButton("Count");
+		cp.add(btnCount);
+		
+		//Allocate an anonymous instance of an anonymous inner class
+		//that implements ActionListener as ActionEvent listener
+		
+		btnCount.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				++count;
+				tfCount.setText(count +"");
 			}
 		});
 		
-		f.setContentPane(new ArtGeo());
-		f.setSize(400, 400);
-		f.setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Program is terminated when the window is closed
+		setTitle("Swing Counter"); //"this" JFrame sets title
+		setSize(300, 100); //"this" JFrame sets initial size
+		setVisible(true); //"this" JFrame shows		
 	}
-
+	
+	public static void main(String[] args){
+		//Runs the GUI construct in the event-dispatching thread for thread-safety
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new ArtGeo(); //Constructor does the job!				
+			}
+		});
+	}	
 }
